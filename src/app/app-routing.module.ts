9b7826e12c0c18d8816e8b77e3cc16f88941b905
Daeseob Lim <thedaysob@gmail.com>
2019-08-15 22:17:28 -0400

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MainContainerComponent } from './main-container/main-container.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: 'main-container', component: MainContainerComponent },
+  { path: '',   redirectTo: '/main-container', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MainContainerComponent } from './main-container/main-container.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MapComponent } from './map/map.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContainerComponent,
+    PageNotFoundComponent,
+    MapComponent,
+    SearchBarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyC2DYCevqVMS4gaVhigniVcUdC1wntPUhM'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
